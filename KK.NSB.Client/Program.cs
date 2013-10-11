3fd20c196a0b7fc4d30b8ea5401793974bea0afb
kkurni <kkurni@gmail.com>
2013-10-11 18:09:34 +1100

    Initital client

---
+++ KK.NSB.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KK.NSB.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
